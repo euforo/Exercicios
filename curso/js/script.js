bbbdd19727e8c3df7659618126dbda8771d4296c
Rofoeu <74689138+euforo@users.noreply.github.com>
2021-04-26 17:01:54 -0300

    Correção de bug
    Correção de bug.

--- curso/js/script.js
+++ curso/js/script.js
@@ -4,13 +4,14 @@ function carregar(){
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('imagem');
     var data = new Date();
+    var horario = data.getHours();
     var hora = data.toLocaleTimeString();
     msg.innerHTML = `Agora são ${hora} horas.`
-    var timer=setInterval(hora,1000);
-    if (hora >= 0 && hora < 12){
+   
+    if (horario >= 0 && horario < 12){
         img.src ="img/manha.png"
         document.body.style.background = '#e2cd9f';
-    }else if (hora >=12 && hora <18){
+    }else if (horario >=12 && horario <18){
         img.src ="img/tarde.png"
         document.body.style.background = '#b9846f';
     }else{
